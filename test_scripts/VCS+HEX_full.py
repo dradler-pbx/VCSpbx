3158ca215191a1c181baa6afe7dae857a0734946
dradler-pbx <dominik.radler@productbloks.com>
2022-03-10 10:08:16 +0100

    HEX added to full system

---
+++ test_scripts/VCS+HEX_full.py
@@ -0,0 +1,53 @@
+import VCSpbx as vcs
+from CoolProp.CoolProp import PropsSI as CPPSI
+
+# parameter setting
+cpr_speed = 2400.0
+T_amb = 30. + 273.15
+ref = 'R290'
+sl = 'INCOMP::MEG[0.5]'
+air = 'AIR'
+h_in_sl = CPPSI('H', 'P', 1e5, 'T', 273.15 - 1.85, sl)
+mdot_SL = 0.25
+mdot_box = 0.5
+T_box = 4 + 273.15
+h_air_box = CPPSI('H', 'T', T_box, 'P', 1e5, air)
+p_SL = 1e5
+superheat = 4.0
+
+mdot_init = 4e-3
+pc_init = 14e5
+Tc_init = CPPSI('T', 'P', pc_init, 'Q', 0, ref)
+h2_init = CPPSI('H', 'P', pc_init, 'T', 60+273.15, ref)
+h3_init = CPPSI('H', 'P', pc_init, 'Q', 0, ref)
+h4_init = CPPSI('H', 'P', pc_init, 'T', Tc_init-2., ref)
+p0_init = 2.5e5
+T0_init = CPPSI('T', 'P', p0_init, 'Q', 1, ref)
+h5_init = CPPSI('H', 'P', p0_init, 'T', T0_init+superheat, ref)
+h1_init = CPPSI('H', 'P', p0_init, 'T', T0_init+superheat+2., ref)
+h_out_SL_init = CPPSI('H', 'P', 1e5, 'T', 273.15 - 10.0, sl)
+
+# components
+system = vcs.System(id='system', tolerance=1e-4)
+cpr = vcs.Compressor_efficiency(id='cpr', system=system, etaS=0.65, etaV=0.9, stroke=33e-6, speed=cpr_speed)
+cond = vcs.Condenser(id='cond', system=system, k=[450., 450., 450.], area=1., subcooling=0.1, T_air_in=T_amb, mdot_air_in=0.56)
+ihx = vcs.IHX(id='ihx', system=system, UA=2.3)
+evap = vcs.Evaporator(id='evap', system=system, k=[420., 420.], area=1., superheat=superheat, boundary_switch=True, limit_temp=True)
+hex = vcs.HeatExchanger(id='hex', system=system, UA=100.)
+srcAir = vcs.Source(id='srcAir', system=system, mdot=mdot_box, p=1e5, h=h_air_box)
+snkAir = vcs.Sink(id='snkAir', system=system)
+
+# junctions ref cycle
+cpr_cond = vcs.Junction(id='cpr_cond', system=system, medium=ref, upstream_component=cpr, upstream_id='outlet_A', downstream_component=cond, downstream_id='inlet_A', mdot_init=mdot_init, p_init=pc_init, h_init=h2_init)
+cond_ihx = vcs.Junction(id='cond_ihx', system=system, medium=ref, upstream_component=cond, upstream_id='outlet_A', downstream_component=ihx, downstream_id='inlet_A', mdot_init= mdot_init, p_init=pc_init, h_init=h3_init)
+ihx_evap = vcs.Junction(id='ihx_evap', system=system, medium=ref, upstream_component=ihx, upstream_id='outlet_A', downstream_component=evap, downstream_id='inlet_A', mdot_init= mdot_init, p_init=p0_init, h_init=h4_init)
+evap_ihx = vcs.Junction(id='evap_ihx', system=system, medium=ref, upstream_component=evap, upstream_id='outlet_A', downstream_component=ihx, downstream_id='inlet_B', mdot_init=mdot_init, p_init=p0_init, h_init=h5_init)
+ihx_cpr = vcs.Junction(id='ihx_cpr', system=system, medium=ref, upstream_component=ihx, upstream_id='outlet_B', downstream_component=cpr, downstream_id='inlet_A', mdot_init=mdot_init, p_init=p0_init, h_init=h1_init)
+
+# junctions glycol loop
+hex_evap = vcs.Junction(id='srcSL_evap', system=system, medium=sl, upstream_component=hex, upstream_id='outlet_A', downstream_component=evap, downstream_id='inlet_B', mdot_init=mdot_SL, p_init=p_SL, h_init=h_in_sl)
+evap_hex = vcs.Junction(id='evap_snkSL', system=system, medium=sl, upstream_component=evap, upstream_id='outlet_B', downstream_component=hex, downstream_id='inlet_A', mdot_init=mdot_SL, p_init=p_SL, h_init=h_out_SL_init)
+srcAir_hex = vcs.Junction(id='srcAir_hex', system=system, medium=air, upstream_component=srcAir, upstream_id='outlet_A', downstream_component=hex, downstream_id='inlet_B', mdot_init=mdot_box, p_init=1e5, h_init=h_air_box)
+hex_snkAir = vcs.Junction(id='hex_snkAir', system=system, medium=air, upstream_component=hex, upstream_id='outlet_B', downstream_component=snkAir, downstream_id='inlet_A', mdot_init=mdot_box, p_init=1e5, h_init=h_air_box)
+
+system.run(full_output=True)
